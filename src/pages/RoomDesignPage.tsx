2a1292e1ddd4511c6c02ed06e87820f222fe25cb
fjoralba29 <90421671+fjoralba29@users.noreply.github.com>
2026-02-16 15:15:08 +0100

    Updated RoomDesignPage.tsx

--- src/pages/RoomDesignPage.tsx
+++ src/pages/RoomDesignPage.tsx
@@ -60,7 +60,7 @@ export default function RoomDesignPage() {
             Dizajni i Dhomës
           </h1>
           <p className="text-xl text-gray-300">
-            Klikoni në çdo sipërfaqe për të aplikuar mermerin e zgjedhur
+            Klikoni në çdo sipërfaqe për të zgjedhur mermerin
           </p>
         </div>
       </div>
